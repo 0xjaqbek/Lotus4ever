fd60dec70521eb4082adc7b08cdaaadf9a7dd91c
0xjaqbek <jaqbek.eth@gmail.com>
2024-09-04 20:42:20 +0200

    touch

--- script.js
+++ script.js
@@ -737,27 +737,31 @@ const muteButton = document.querySelector('.mute-button');
 const startButton = document.querySelector('.start-button');
 const exitButton = document.querySelector('.exit-button');
 
-goButton.addEventListener('mousedown', () => {
+let goButtonPressed = false;
+
+goButton.addEventListener('touchstart', () => {
+  goButtonPressed = true;
   KEYS.ArrowUp = true;
 });
 
-goButton.addEventListener('mouseup', () => {
-  KEYS.ArrowUp = true;
+goButton.addEventListener('touchend', () => {
+  goButtonPressed = false;
+  KEYS.ArrowUp = false;
 });
 
-leftButton.addEventListener('mousedown', () => {
+leftButton.addEventListener('touchstart', () => {
   KEYS.ArrowLeft = true;
 });
 
-leftButton.addEventListener('mouseup', () => {
+leftButton.addEventListener('touchend', () => {
   KEYS.ArrowLeft = false;
 });
 
-rightButton.addEventListener('mousedown', () => {
+rightButton.addEventListener('touchstart', () => {
   KEYS.ArrowRight = true;
 });
 
-rightButton.addEventListener('mouseup', () => {
+rightButton.addEventListener('touchend', () => {
   KEYS.ArrowRight = false;
 });
 
@@ -803,3 +807,14 @@ startButton.addEventListener('click', () => {
 exitButton.addEventListener('click', () => {
   reset();
 });
+
+// Update the `update()` function to handle the GO button
+function update(step) {
+  // ...
+  if (goButtonPressed) {
+    KEYS.ArrowUp = true;
+  } else {
+    KEYS.ArrowUp = false;
+  }
+  // ...
+}
