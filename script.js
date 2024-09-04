fb4416df0e1e3abc85ab203437395603c78fd31c
0xjaqbek <jaqbek.eth@gmail.com>
2024-09-04 20:37:02 +0200

    first controls

--- script.js
+++ script.js
@@ -725,4 +725,81 @@ function init() {
 init();
 //# sourceURL=pen.js
 
+// ------------------------------------------------------------
+// MOBILE CONTROLS
+// ------------------------------------------------------------
 
+// Add event listeners for mobile controls
+const goButton = document.querySelector('.go-button');
+const leftButton = document.querySelector('.left-button');
+const rightButton = document.querySelector('.right-button');
+const muteButton = document.querySelector('.mute-button');
+const startButton = document.querySelector('.start-button');
+const exitButton = document.querySelector('.exit-button');
+
+goButton.addEventListener('mousedown', () => {
+  KEYS.ArrowUp = true;
+});
+
+goButton.addEventListener('mouseup', () => {
+  KEYS.ArrowUp = true;
+});
+
+leftButton.addEventListener('mousedown', () => {
+  KEYS.ArrowLeft = true;
+});
+
+leftButton.addEventListener('mouseup', () => {
+  KEYS.ArrowLeft = false;
+});
+
+rightButton.addEventListener('mousedown', () => {
+  KEYS.ArrowRight = true;
+});
+
+rightButton.addEventListener('mouseup', () => {
+  KEYS.ArrowRight = false;
+});
+
+muteButton.addEventListener('click', () => {
+  if (KEYS.KeyM) {
+    KEYS.KeyM = false;
+  } else {
+    KEYS.KeyM = true;
+  }
+  audio.volume = audio.volume === 0 ? 1 : 0;
+});
+
+startButton.addEventListener('click', () => {
+  if (!inGame) {
+    sleep(0)
+      .then((_) => {
+        text.classList.remove('blink');
+        text.innerText = 3;
+        audio.play('beep');
+        return sleep(1000);
+      })
+      .then((_) => {
+        text.innerText = 2;
+        audio.play('beep');
+        return sleep(1000);
+      })
+      .then((_) => {
+        reset();
+
+        home.style.display = 'none';
+
+        road.style.opacity = 1;
+        hero.style.display = 'block';
+        hud.style.display = 'block';
+
+        audio.play('beep', 500);
+
+        inGame = true;
+      });
+  }
+});
+
+exitButton.addEventListener('click', () => {
+  reset();
+});
