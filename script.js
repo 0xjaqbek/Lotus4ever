57f6ce4c148b648e996bde5f9fbbf4a77e88ebae
0xjaqbek <jaqbek.eth@gmail.com>
2024-09-07 18:39:06 +0200

    script move

--- script.js
+++ script.js
@@ -5,7 +5,16 @@ let db; // Declare `db` at a higher scope
 
 window.onload = function() {
   // Initialize Firebase inside an event that ensures the page has loaded
-  const firebaseConfig = { /* your Firebase config */ };
+  const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+  };
+  
   firebase.initializeApp(firebaseConfig);
   db = firebase.database(); // Initialize the `db` here
 
@@ -883,15 +892,6 @@ const ASSETS = {
     reset();
 });
 
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-};
 
 
 // Submit time function - improved to handle numeric time comparison
