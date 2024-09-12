50cc5d4174c6d439b446e02a92df5e38be8bc6ee
0xjaqbek <jaqbek.eth@gmail.com>
2024-09-12 11:45:40 +0200

    101

--- script.js
+++ script.js
@@ -873,13 +873,13 @@ const ASSETS = {
 });
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  apiKey: "AIzaSyCHuPCcZBPHaoov-GnN0uX5VPfNHGs8q4g",
+  authDomain: "lotus-8fa6e.firebaseapp.com",
+  databaseURL: "https://lotus-8fa6e-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "lotus-8fa6e",
+  storageBucket: "lotus-8fa6e.appspot.com",
+  messagingSenderId: "42734428096",
+  appId: "1:42734428096:web:8e1b839cdcff2e9b737225",
 };
 
 let db; // Declare `db` at a higher scope
